4c474c60aa2fbbe0f3d17ad23109286d31fde57e
Alfred Shaheen <shaheenalfred48@gmail.com>
2022-10-12 09:30:54 -0700

    commit tree 417217dc2e06ee066826fe8524bbe2e4b166850a

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance, charge){
+ 
+  if (distance <= 400){
+    charge = 0
+    return 'This one is on me!'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (distance > 400 && distance < 2000){
+    charge = 20
+    return 'That will be twenty bucks.'
+} else if (distance > 2000 && distance < 2500){
+    return "I will gladly take your thirty bucks.";
+}
+else if(distance > 2500){
+  return "No can do.";
+
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.';
+
+}
+
+function switchOnCharmFromTip(tip){
+ switch (tip){
+  case `generous`:
+    return "Thank you so much."
+  case 'not as generous':
+    return 'Thank you.'
+  default: return 'Bye.' 
+ }
 }
